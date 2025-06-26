2a23e06546fbef1e11ec724802708798c6d3cc0c
Vinicius Penedo <Viniciuspenedo2003@gmail.com>
2025-06-25 22:17:38 -0300

    Base lite

--- vendor/ramsey/uuid/src/Rfc4122/UuidV6.php
+++
@@ -1,7 +0,0 @@
-xFrame: To many senders for Channel 0 Port 0
-10:16:22,220 pniopcac.exe_Wow64 (009452|011156): [1|55|4]; lldp_usr.c(1796); LLDP_UserHandleRxFrame: To many senders for Channel 0 Port 0
-10:16:23,239 pniopcac.exe_Wow64 (009452|011156): [1|55|4]; lldp_usr.c(1796); LLDP_UserHandleRxFrame: To many senders for Channel 0 Port 0
-10:16:27,220 pniopcac.exe_Wow64 (009452|011156): [1|55|4]; lldp_usr.c(1796); LLDP_UserHandleRxFrame: To many senders for Channel 0 Port 0
-10:16:28,243 pniopcac.exe_Wow64 (009452|011156): [1|55|4]; lldp_usr.c(1796); LLDP_UserHandleRxFrame: To many senders for Channel 0 Port 0
-10:16:32,216 pniopcac.exe_Wow64 (009452|011156): [1|55|4]; lldp_usr.c(1796); LLDP_UserHandleRxFrame: To many senders for Channel 0 Port 0
-10:16:33,236 pniopcac.exe_Wow64 (009452|
